953ed9c68fae1160b261f17bf06047136c6cea20
maneeshparihar <maneeshparihar@gmail.com>
2020-04-23 15:13:51 +0530

    auto generates mp4 files

--- index.php
+++ index.php
@@ -24,9 +24,9 @@
       <video id="my-video" class="video-js" controls preload="auto" width="640" height="264"
           data-setup="{}">
           <source src="<?php echo basename($file)?>" type="video/mp4" />
-          <p class="vjs-no-js"> To view this video please enable JavaScript, and consider upgrading to a
+          <!-- <p class="vjs-no-js"> To view this video please enable JavaScript, and consider upgrading to a
             web browser that <a href="https://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
-          </p>
+          </p> -->
       </video>
       </div>
     </div>
